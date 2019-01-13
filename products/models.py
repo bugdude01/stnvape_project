4f4f8da7815c33b67ed774f3bf981c3212d180cc
bugdude01 <fafrhodes@yahoo.co.uk>
2019-01-13 10:31:40 +0000

    added products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+    def __str__(self):
+        return self.title

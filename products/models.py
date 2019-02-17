61b982b09ca502a3fb7bbbb00cf053ca7ee6532e
bugdude01 <fafrhodes@yahoo.co.uk>
2019-02-17 22:12:48 +0000

    general updates

--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-
     def __str__(self):
         return self.title
+
+    def __summary__(self):
+        return self.body[:50]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'products/home.html')
+
+def style(request):
+    return render(request, 'products/style.html')
